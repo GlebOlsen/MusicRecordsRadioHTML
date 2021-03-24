7fc8fbab6c5689b21cff1ae28901824d6a67a201
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2021-03-24 14:11:56 +0100

    Added js to get data from rest

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const baseUrl = "https://pairprogram-radiorest.azurewebsites.net/api/radios"
+
 Vue.createApp({
+    data(){
+        return{
+            musicrecords: []
+        }
+    },
+    methods: {
+        getAllRecords(){
+            this.helperGetAndShow(baseUrl)
+        },
+        async helperGetAndShow(url){
+            try{
+                const response = await axios.get(url)
+                this.musicrecords = await response.data
+            } catch (ex) {
+                alert(ex.message)
+            }
+        }
+    }
     
 }).mount("#app")
